422db5d72ee53882fc05a8e9aca0aa0d987e5519
TheDevGoSike283 <111324250+TheDevGoSike283@users.noreply.github.com>
2022-08-16 11:24:39 -0400

    Update espeakcode.sh

--- usr/bin/espeakcode.sh
+++ usr/bin/espeakcode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "\033[33;34m Type in anything and it will be said out loud."
+echo "\033[33;34mType in anything and it will be said out loud."
 speak=nothing
 read speak
-for i in 3 2 1; do echo "\033[33;33m Installing \"espeak\" in $i second(s)"; sleep 1; done;
+for i in 3 2 1; do echo "\033[33;33mInstalling \"espeak\" in $i second(s)"; sleep 1; done;
 sudo apt install espeak;
 espeak "$speak"
