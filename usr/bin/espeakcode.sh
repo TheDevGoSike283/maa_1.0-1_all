5986b43bbf0689de5440b80531efaa4e473980b1
TheDevGoSike283 <111324250+TheDevGoSike283@users.noreply.github.com>
2022-08-16 12:01:26 -0400

    Update espeakcode.sh

--- usr/bin/espeakcode.sh
+++ usr/bin/espeakcode.sh
@@ -2,6 +2,9 @@
 echo "\033[33;34mType in anything and it will be said out loud."
 speak=nothing
 read speak
+echo "\033[33;34mWhat speed? Has to be in words per minute."
+textspeed=nothing
+read textspeed
 for i in 3 2 1; do echo "\033[33;33mInstalling \"espeak\" in $i second(s)"; sleep 1; done;
 sudo apt install espeak;
-espeak "$speak"
+espeak -s $textspeed "$speak"
