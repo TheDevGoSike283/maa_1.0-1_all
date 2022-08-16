d099c906e3e0654e95315aa16062d3126b921a1c
TheDevGoSike283 <111324250+TheDevGoSike283@users.noreply.github.com>
2022-08-16 11:53:21 -0400

    Update rainbowcode.sh

--- usr/bin/rainbowcode.sh
+++ usr/bin/rainbowcode.sh
@@ -2,6 +2,6 @@
 rainbowtext=nothing
 echo "\033[33;34mType in something and it will be rainbow text."
 read rainbowtext
-for i in 3 2 1; do echo "\033[33;33mInstalling \"toilet\" (linux font command) in $i second(s)"; done;
+for i in 3 2 1; do echo "\033[33;33mInstalling \"toilet\" (linux font command) in $i second(s)"; sleep 1; done;
 sudo apt install toilet
 toilet --gay -f future "$rainbowtext" 
