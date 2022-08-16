c1a189a617fb2621df4cb5950ded194142a40b07
TheDevGoSike283 <111324250+TheDevGoSike283@users.noreply.github.com>
2022-08-16 10:44:08 -0400

    Create colors.h

---
+++ usr/bin/colors.h
@@ -0,0 +1,8 @@
+#define BLK "\e[0;30m"
+#define RED "\e[4;31m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
+#define GRN "\e[0;32m"
